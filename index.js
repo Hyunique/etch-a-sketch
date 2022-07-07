e01f63c8391a0a1a3eb0371d3570e7f49f31e611
Elena <kunstvanelena@gmail.com>
2022-07-07 12:15:05 +0200

    fixed click event bug

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 
 let container = document.querySelector('.boxContainer')
+
+/* Create 16*16 grid and add it in container div*/
 for (let i = 0; i < 256; i++) {
     const boxes = document.createElement('div')
     container.appendChild(boxes)
+    boxes.classList.add('boxes')
 }
-document.body.insertBefore(boxes, container)
+
+/* Select the grid and add an click event to change color */
+const boxes = document.querySelectorAll('.boxes')
+for (let box of boxes) {
+    box.addEventListener('click', function () {
+        box.style.backgroundColor = 'black'
+    })
+}

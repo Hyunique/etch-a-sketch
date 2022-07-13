fadc3a9778d8ea3582ba8e4b1057905e217ac47b
Elena <kunstvanelena@gmail.com>
2022-07-13 14:26:21 +0200

    Added function for choosing grid size

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 
+/* Default grid */
 let container = document.querySelector('.boxContainer')
-let mouseDown = 0;
-document.body.onmousedown = () => mouseDown = 1;
-document.body.onmouseup = () => mouseDown = 0;
-
-/* Create 16*16 grid and add it in container div*/
-for (let i = 0; i < 256; i++) {
+let gridNum = 4
+for (let i = 0; i < gridNum * gridNum; i++) {
     const boxes = document.createElement('div')
     container.appendChild(boxes)
     boxes.classList.add('boxes')
+    colorGrid()
 }
 
-/* Select the grid and add an click event to change color */
-const boxes = document.querySelectorAll('.boxes')
-const eraseBar = document.querySelector('.resetlabel')
-for (let box of boxes) {
-    box.addEventListener('mouseover', () => {
-        if (mouseDown) {
-            // box.style.backgroundColor = 'rgb(38, 38, 38)'
-            box.classList.add('colored')
-        }
-    })
-    eraseBar.addEventListener('click', () => {
-        box.classList.remove('colored')
-    })
+let grid4 = document.querySelector('.grid4')
+let grid8 = document.querySelector('.grid8')
+let grid16 = document.querySelector('.grid16')
+let gridArr = [grid4, grid8, grid16]
+
+for (let grid of gridArr) {
+    grid.addEventListener('click', removeGrid)
+    grid.addEventListener('click', createGrid)
+
 }
+
+/* Function to create box and put it in grid */
+function createGrid() {
+    gridNum = this.getAttribute('value')
+    for (let i = 0; i < gridNum * gridNum; i++) {
+        const boxes = document.createElement('div')
+        container.appendChild(boxes)
+        boxes.classList.add('boxes')
+        container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+        colorGrid()
+    }
+}
+
+/* Select the box grid and add a click event to change color */
+/* When mouse is clicked && moving, color the box */
+function colorGrid() {
+    const boxes = document.querySelectorAll('.boxes')
+    let mouseDown = 0;
+    document.body.onmousedown = () => mouseDown = 1;
+    document.body.onmouseup = () => mouseDown = 0;
+
+    for (let box of boxes) {
+        box.addEventListener('mouseover', () => {
+            if (mouseDown) {
+                box.classList.add('colored')
+            }
+        })
+
+    }
+}
+
+const eraseBar = document.querySelector('.resetlabel')
+eraseBar.addEventListener('click', eraseColor)
+
+function eraseColor() {
+    let box = document.querySelectorAll('.boxes')
+    box.classList.remove('colored')
+}
+function removeGrid() {
+    let boxes = container.querySelectorAll('div')
+    boxes.forEach(boxes => boxes.remove());
+}
+/* 14th July _ Need to modify radio buttons to range input
+                Make color function to be able with individual click */
+
+
 
 

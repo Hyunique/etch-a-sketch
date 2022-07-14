56320dd330cf221d95b0c92fc1b1560716de7c45
Elena <kunstvanelena@gmail.com>
2022-07-14 14:04:02 +0200

    Added rainbow color function

--- index.js
+++ index.js
@@ -24,36 +24,61 @@ function createGrid(gridNum) {
 
 /* Select the box grid and add a click event to change color */
 /* When mouse is clicked && moving, color the box */
+let mouseDown = 0;
+document.body.onmousedown = () => mouseDown = 1;
+document.body.onmouseup = () => mouseDown = 0;
+
 function colorGrid() {
     const boxes = document.querySelectorAll('.boxes')
-    let mouseDown = 0;
-    document.body.onmousedown = () => mouseDown = 1;
-    document.body.onmouseup = () => mouseDown = 0;
-
     for (let box of boxes) {
         box.addEventListener('mouseover', () => {
             if (mouseDown) {
                 box.classList.add('colored')
             }
         })
+        box.addEventListener('click', () => box.classList.add('colored'))
     }
 }
 
+let rainbowBtn = document.querySelector('.rainbow')
+rainbowBtn.addEventListener('input', colorRainbow)
+function colorRainbow() {
+    const boxes = document.querySelectorAll('.boxes')
+    const rainbowColor = makeRandomColor()
+    for (let box of boxes) {
+        box.addEventListener('mouseover', () => {
+            if (mouseDown) {
+                box.style.backgroundColor = rainbowColor
+            }
+        })
+        box.addEventListener('click', () => box.style.backgroundColor = rainbowColor)
+    }
+}
 
+function makeRandomColor() {
+    const r = Math.floor(Math.random() * 255)
+    const g = Math.floor(Math.random() * 255)
+    const b = Math.floor(Math.random() * 255)
+    return `rgb(${r},${g},${b})`
+}
+
+let darkModeBtn = document.querySelector('.dark')
+
+/* Function for erasing the whole canvas */
 function eraseColor() {
     boxes = document.querySelectorAll('.boxes')
-    for (let box of boxes) {
-        box.classList.remove('colored')
-    }
-
-    // boxes.forEach(boxes => boxes.classList.remove('colored'));
+    boxes.forEach(boxes => boxes.classList.remove('colored'));
 }
 
+/* Remove current grid */
 function removeGrid() {
     let boxes = container.querySelectorAll('div')
     boxes.forEach(boxes => boxes.remove());
 }
-/* 14th July _  Erase bar!!! 
-                Make color function to be able with individual click */
+/* 14th July _  
+Rainbow color each cell
+R
+Glow-in-dark function
+Clean up repeating queryselector*/
 
 

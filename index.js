a0b65ed111566e6c82dc98294b19272d3d2ee644
Elena <kunstvanelena@gmail.com>
2022-07-15 17:20:15 +0200

    wrote cleaner code for coloring function

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 let container = document.querySelector('.boxContainer')
 let gridController = document.querySelector('#gridSize')
 gridController.addEventListener('input', createGrid)
-
+let mouseDown = 0;
+document.body.onmousedown = () => mouseDown = 1;
+document.body.onmouseup = () => mouseDown = 0;
 createGrid(4)
+
 const eraseBar = document.querySelector('.resetlabel')
 eraseBar.addEventListener('click', eraseColor)
+
+
 
 /* Function to create box and put it in grid */
 function createGrid(gridNum) {
@@ -19,39 +24,34 @@ function createGrid(gridNum) {
         container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
     }
-    colorGrid()
+
 }
 
-/* Select the box grid and add a click event to change color */
-/* When mouse is clicked && moving, color the box */
-let mouseDown = 0;
-document.body.onmousedown = () => mouseDown = 1;
-document.body.onmouseup = () => mouseDown = 0;
-
-function colorGrid() {
-    const boxes = document.querySelectorAll('.boxes')
-    for (let box of boxes) {
-        box.addEventListener('mouseover', () => {
-            if (mouseDown) {
-                box.classList.add('colored')
+/* color grid () */
+let boxes = document.querySelectorAll('.boxes')
+for (let box of boxes) {
+    box.addEventListener('mouseover', () => {
+        if (mouseDown) {
+            getColorMode()
+            if (modeValue === 'black') {
+                box.style.backgroundColor = 'black'
+            } else if (modeValue === 'rainbow') {
+                console.log("Rainbow!")
+                box.style.backgroundColor = makeRandomColor()
+            } else if (modeValue === 'dark') {
+                box.style.backgroundColor = '#fff009'
             }
-        })
-        box.addEventListener('click', () => box.classList.add('colored'))
-    }
+        }
+    })
 }
 
-let rainbowBtn = document.querySelector('.rainbow')
-rainbowBtn.addEventListener('input', colorRainbow)
-function colorRainbow() {
-    const boxes = document.querySelectorAll('.boxes')
-    const rainbowColor = makeRandomColor()
-    for (let box of boxes) {
-        box.addEventListener('mouseover', () => {
-            if (mouseDown) {
-                box.style.backgroundColor = rainbowColor
-            }
-        })
-        box.addEventListener('click', () => box.style.backgroundColor = rainbowColor)
+/* Get the value of radio button when checked */
+function getColorMode() {
+    let colorModes = document.querySelectorAll('#colorMode')
+    for (i = 0; i < colorModes.length; i++) {
+        if (colorModes[i].checked) {
+            return modeValue = colorModes[i].getAttribute('value')
+        }
     }
 }
 
@@ -62,12 +62,10 @@ function makeRandomColor() {
     return `rgb(${r},${g},${b})`
 }
 
-let darkModeBtn = document.querySelector('.dark')
-
 /* Function for erasing the whole canvas */
 function eraseColor() {
     boxes = document.querySelectorAll('.boxes')
-    boxes.forEach(boxes => boxes.classList.remove('colored'));
+    boxes.forEach(boxes => boxes.style.backgroundColor = '');
 }
 
 /* Remove current grid */
@@ -75,10 +73,55 @@ function removeGrid() {
     let boxes = container.querySelectorAll('div')
     boxes.forEach(boxes => boxes.remove());
 }
-/* 14th July _  
-Rainbow color each cell
-R
-Glow-in-dark function
-Clean up repeating queryselector*/
+/*Original code************************************************* */
+/* Select the box grid and add a click event to change color */
+/* When mouse is clicked && moving, color the box */
+
+// let blackBtn = document.querySelector('.black')
+// blackBtn.addEventListener('click', colorGrid)
+// function colorGrid() {
+//     const boxes = document.querySelectorAll('.boxes')
+//     for (let box of boxes) {
+//         box.addEventListener('mouseover', () => {
+//             if (mouseDown) {
+//                 box.style.backgroundColor = 'black'
+//                 // box.classList.add('colored')
+//             }
+//         })
+//         box.addEventListener('click', () => box.style.backgroundColor = 'black')
+//     }
+// }
+
+// let rainbowBtn = document.querySelector('.rainbow')
+// rainbowBtn.addEventListener('click', colorRainbow)
+// function colorRainbow() {
+//     const boxes = container.querySelectorAll('div')
+//     for (let box of boxes) {
+//         box.addEventListener('mouseover', () => {
+//             if (mouseDown) {
+//                 box.style.backgroundColor = makeRandomColor()
+//             }
+//         })
+//         box.addEventListener('click', () => box.style.backgroundColor = makeRandomColor())
+//     }
+// }
 
 
+
+// let darkModeBtn = document.querySelector('.dark')
+// darkModeBtn.addEventListener('click', colorFluore)
+// function colorFluore() {
+//     let boxes = container.querySelectorAll('div')
+//     for (let box of boxes) {
+//         box.addEventListener('mouseover', () => {
+//             if (mouseDown) {
+//                 box.classList.add('fluore')
+//             }
+//         })
+//         box.addEventListener('click', () => box.classList.add('fluore'))
+//     }
+// }
+/************************************************* */
+
+
+/* 15th July _  color grid () is not working when new grid is made*/
